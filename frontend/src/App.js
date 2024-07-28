88f12c3f25c05ad88a0efe90b0ae4c6e957a1390
Kiet Cao <kietcao@tuta.io>
2024-07-28 20:59:38 +0900

    use loader to load events

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import EventsPage from "./pages/EventsPage";
+import EventsPage, {loader as eventsLoader} from "./pages/EventsPage";
 import EventDetailPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
@@ -44,6 +44,7 @@ const router = createBrowserRouter([
           {
             path: "",
             element: <EventsPage />,
+            loader: eventsLoader,
           },
           {
             path: ":eventId",
--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,24 +1,25 @@
-import EventsList from "../components/EventsList";
-const EventsPage = () => {
-  const events = [{
-    id: "1",
-    image: "",
-    title: "Event 1",
-    date: "2022-12-12",
-  },
-    {
-      id: "2",
-      image: "",
-      title: "Event 2",
-      date: "2022-12-14",
-    }
-  ];
+import {useLoaderData}  from 'react-router-dom';
+
+import EventsList from '../components/EventsList';
+
+function EventsPage() {
+
+  const events = useLoaderData();
+
   return (
-    <div>
-      <div>Events Page</div>
-      <EventsList events={events} />
-    </div>
+    <EventsList events={events} /> 
   );
-};
+}
 
 export default EventsPage;
+
+export const loader =  async () => {
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+    // 
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
